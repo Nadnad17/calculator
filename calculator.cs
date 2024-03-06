8fb9a54a92e09fd9c97ac2388a065a6b53f59188
psnadia21@gmail.com <psnadia21@gmail.com>
2024-03-06 16:56:32 +0700

    Menambahkan file Calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -3,6 +3,11 @@ class calculator
 static void Main(string[] args)
 
 {
-	                                                             
+	
 }
+static int penambahan(int a, int b)
+{
+	retrun a+b;
 }
+
+} 
